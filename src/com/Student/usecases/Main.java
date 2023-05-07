86cafbd62ff5f30e5eb0988bf5ebf7bbfd53df69
krajeet35 <krajeet35@gmail.com>
2023-05-07 16:26:51 +0530

    completed

--- src/com/Student/usecases/Main.java
+++ src/com/Student/usecases/Main.java
@@ -13,6 +13,7 @@ import com.Student.model.Student;
 public class Main {
 
 	public static void main(String[] args) {
+		System.out.println("Welcome to the Automated Student Registration System");
 		
 		Scanner sc= new Scanner(System.in);
 		
